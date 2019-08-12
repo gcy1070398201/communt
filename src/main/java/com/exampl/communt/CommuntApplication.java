a88756993a6a5e67086c766d45c926a9c3d9b8d3
gcy <gu1070398201@sina.com>
2019-08-12 12:33:17 +0800

    Initial commit

---
+++ src/main/java/com/exampl/communt/CommuntApplication.java
@@ -0,0 +1,13 @@
+package com.exampl.communt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuntApplication.class, args);
+	}
+
+}
