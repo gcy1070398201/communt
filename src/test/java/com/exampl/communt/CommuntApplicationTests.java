98cfd3783643474d597f51f0a68b343b5f9dacf2
gcy <gu1070398201@sina.com>
2019-08-12 13:35:26 +0800

    提交测试

---
+++ src/test/java/com/exampl/communt/CommuntApplicationTests.java
@@ -0,0 +1,16 @@
+package com.exampl.communt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommuntApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
