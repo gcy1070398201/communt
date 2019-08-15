6ddf89546f86e50aff046c6ea7337147194f575e
gcy <gu1070398201@sina.com>
2019-08-15 18:18:36 +0800

    1.修改 publish.html 2.写publish Controller

--- src/main/java/com/exampl/communt/controller/PublishController.java
+++ src/main/java/com/exampl/communt/controller/PublishController.java
@@ -2,6 +2,8 @@ package com.exampl.communt.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PublishController {
@@ -9,6 +11,21 @@ public class PublishController {
     @GetMapping(value = "/publish")
     public String publish(){
         return"publish";
+    }
+
+    /**
+     * 提交问题
+     * @param title
+     * @param describe
+     * @param label
+     * @return
+     */
+    @PostMapping(value ="/publish")
+    public String publish(@RequestParam String title,
+                          @RequestParam String describe,
+                          @RequestParam String label){
+
+        return "";
     }
 
 }
---
+++ src/main/java/com/exampl/communt/mode/PublishMode.java
@@ -0,0 +1,12 @@
+package com.exampl.communt.mode;
+
+import java.io.Serializable;
+
+public class PublishMode implements Serializable {
+    private Integer id;
+    private String title;
+    private String describe;
+    private String label;
+    private long gmtCreate;
+    private long gmtModified;
+}
